867e5d058fd55419cf14822e84704154039e9a54
Pablo Tejada <ptejada26@gmail.com>
2012-05-05 00:45:32 -0400

    Simplified server script

--- APE_SERVER/scripts/main.ape.js
+++ APE_SERVER/scripts/main.ape.js
@@ -1,17 +1,7 @@
 Ape.addEvent("init", function() {
 	include("framework/mootools.js");
 	include("framework/Http.js");
-	//include("framework/userslist.js");
+	include("framework/userslist.js");
 	include("utils/utils.js");
-	//include("commands/proxy.js");
-	//include("commands/inlinepush.js");
-	//include("examples/nickname.js");
-	//include("examples/move.js");
-	//include("utils/checkTool.js"); //Just needed for the APE JSF diagnostic tool, once APE is installed you can removeit 
-	//clude("examples/ircserver.js");
-	//include("framework/http_auth.js");
-	include("commands/pub.js");
-	include("modules/PubSub.js");
-	include("modules/stockTicker.js");
-	//include("modules/debug.js");
+	include("apsModule.js");
 });
