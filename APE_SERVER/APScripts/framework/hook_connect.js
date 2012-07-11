72917a3f5dda2d3a748ba2db39deb75239c7f493
Pablo Tejada <ptejada26@gmail.com>
2012-07-10 23:08:50 -0400

    Fixed user paramater handling bug

--- APE_SERVER/APScripts/framework/hook_connect.js
+++ APE_SERVER/APScripts/framework/hook_connect.js
@@ -5,8 +5,8 @@ Ape.registerHookCmd("connect", function(params, cmd){
 	if(!params) return 1;
 	
 	if(params.user){
-		
-		if (params.user.name.length > 16 || /[^_a-zA-Z0-9]/i.test(params.user.name)) return ["006", "BAD_NICK"];
+		if(typeof params.user.name == "string")
+			if(params.user.name.length > 16 || /[^_a-zA-Z0-9]/i.test(params.user.name)) return ["006", "BAD_NICK"];
 		
 		for(var index in params.user){
 			cmd.user.prop(index, params.user[index]);
