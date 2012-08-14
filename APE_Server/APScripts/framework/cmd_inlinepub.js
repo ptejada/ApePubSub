389ae66ad35586bc15172b78dad19cff076f11fa
ptejada <ptejada26@gmail.com>
2012-08-14 12:04:55 -0400

    Simplify inlinepub

--- APE_Server/APScripts/framework/cmd_inlinepub.js
+++ APE_Server/APScripts/framework/cmd_inlinepub.js
@@ -5,23 +5,18 @@ Ape.registerCmd("inlinepub", false, function(params, info) {
 		
 		if(params.to && params.raw && params.data){
 			
-			if(params.toType == "multi"){
-				var to = Ape.getChannelByPubid(params.to);
-			}else{
-				var to = Ape.getUserByPubid(params.to);
-				Ape.log("User to User");
-			}
+			var to = Ape.getPipe(params.to);
 			
-			if (!to.pipe) return ["401", "UNKNOWN_CHANNEL"];
+			if (!!to.pipe) return ["425", "UNKNOWN_RECIPIENT"];
 			
 			//User Source From
-			var user = Ape.getUserByPubid(params.from);
-			if (!user.pipe || !user.pipe) return ["401", "UNKNOWN_USER"];
+			var user = Ape.getPipe(params.from);
+			if (!!user.pipe) return ["424", "UNKNOWN_SENDER"];
 			
-			//Send Data to channel
-			to.pipe.sendRaw(params.raw, params.data, {"from": user.pipe});	
+			//Send Data to the Reccipient
+			to.pipe.sendRaw(params.raw, params.data, {"from": user.pipe});
 			
-			return {"name":"pushed","data":{"status":"ok"}}
+			return {"name":"pushed","data":{"status":"ok"}};
 		} else {
 			return 0;
 		}
