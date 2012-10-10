a2d3eff50d2289076846fba2648ceccde594daf1
ptejada <pablo@crusthq.com>
2012-10-09 22:57:18 -0400

    SYNC cleanup

--- APE_Server/APScripts/framework/cmd_event.js
+++ APE_Server/APScripts/framework/cmd_event.js
@@ -4,11 +4,17 @@ Ape.registerCmd("event", true, function(params, info) {
 	var pipe = Ape.getPipe(params.pipe);
 	
 	if(pipe){
-		var sync = params;
-		sync.chanid = params.pipe;
-		delete sync.pipe;
 		
-		if(!!params.sync) info.sendResponse("SYNC", sync);
+		if(!!params.sync){
+			info.sendResponse("SYNC", {
+				data: params.data,
+				event: params.event,
+				chanid: params.pipe
+			});
+		}
+		
+		delete params.sync;
+		
 		pipe.sendRaw("EVENT", params, {from: info.user.pipe});
 	}
 });
