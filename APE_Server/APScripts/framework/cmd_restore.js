acb4e49c6d564e08e6ac37ad2aca7a63daab51b7
ptejada <ptejada26@gmail.com>
2012-10-10 01:32:52 -0400

    cleanup

--- APE_Server/APScripts/framework/cmd_restore.js
+++ APE_Server/APScripts/framework/cmd_restore.js
@@ -1,6 +1,7 @@
 //Pub Command
 Ape.registerCmd("RESTORE", true, function(params, info) {
-	info.sendResponse("RESTORED", {data:1});
+	return {"name": "RESTORED", "data":{"value":"ok"}}
+	//info.sendResponse("RESTORED", {data:1});
 	return 1;
 });
 
