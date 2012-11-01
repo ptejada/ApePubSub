bc69ba72e3db936d560dfc9a0b604139b8c24c4a
ptejada <ptejada26@gmail.com>
2012-10-31 21:56:26 -0400

    remove usercount property from channel objects

--- APE_Server/APScripts/framework/hook_events.js
+++ APE_Server/APScripts/framework/hook_events.js
@@ -25,36 +25,15 @@ Ape.addEvent('deluser', function(user) {
 Ape.addEvent("mkchan", function(channel) {
 	//Create users property	
 	channel.users = {};
-	
-	channel.prop("userCount", 0);
 });
 
 Ape.addEvent("beforeJoin", function(user, channel) {
-	channel.prop("userCount", parseInt(channel.prop("userCount")) + 1);
-	
 	channel.users[user.prop("pubid")] = user;
 	
 	user.channels[channel.prop("name")] = channel;
-	//Ape.log(channel.userCount +" users In channel "+ channel.getProperty("name"));
 });
 
 Ape.addEvent("left", function(user, channel) {
-	channel.prop("userCount", parseInt(channel.prop("userCount")) - 1);
-	
 	delete channel.users[user.prop("pubid")];
 	delete user.channels[channel.prop("name")];
-	//Ape.log(channel.userCount +" users In channel "+ channel.getProperty("name"));
-});
-
-Ape.addEvent("beforeJoin", function(user, channel) {
-	/*
-	Ape.log("========= TEST ==========");
-	Ape.log("========= User ==========");
-	Ape.log(user.prop("name"));
-	Ape.log(user.prop());
-	Ape.log("======== Channel ========");
-	Ape.log(channel.prop("name"));
-	Ape.log(channel.prop());
-	Ape.log("=======================");
-	*/
 });
