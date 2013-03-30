1e8d17419ab8809eed51c710cab0354d0ddf24fd
ptejada <ptejada26@gmail.com>
2013-03-30 02:27:09 -0400

    Updated eventpush command to return error 425 in case of unkown recipient

--- APE_Server/APScripts/framework/cmd_eventpush.js
+++ APE_Server/APScripts/framework/cmd_eventpush.js
@@ -4,29 +4,32 @@
 Ape.registerCmd("eventpush", false, function(params, info) {
 	if(params.to && params.data){
 		
-		if(params.multi){
-			var recipient = Ape.getChannelByPubid(params.pipe);
-		}else{
-			var recipient = Ape.getUserByPubid(params.pipe);
+		var recipient = Ape.getChannelByPubid(params.to);
+		
+		//Try if the recipient is an user
+		if(!recipient)
+			recipient = Ape.getUserByPubid(params.to);
+		
+		if(recipient){
+			//User Source From
+			var user = Ape.getUserByPubid(params.from);
+			if (typeof user.pipe == "undefined" && params.sessid != user.prop("sessid"))
+				return ["424", "UNKNOWN_SENDER"];
+			
+			//Send Data to the Reccipient
+			recipient.sendEvent(params.data, {"from": user.pipe});
+			
+			Ape.log("");	//IMPORTANT: if this line comented a segfault error crashes the server			
+			if(params.sync){
+				var sync = params.data;
+				sync.chanid = params.to;
+				user.pipe.sendRaw("SYNC", sync);
+			}
+			
+			return {"name":"PUSHED","data":{"value":"ok"}};
 		}
 		
-		if(!!!recipient) return ["425", "UNKNOWN_RECIPIENT"];
-		
-		//User Source From
-		var user = Ape.getUserByPubid(params.from);
-		if (typeof user.pipe == "undefined" && params.sessid != user.prop("sessid"))
-			return ["424", "UNKNOWN_SENDER"];
-		
-		//Send Data to the Reccipient
-		recipient.sendEvent(params.data, {"from": user.pipe});
-		
-		if(!!params.sync){
-			var sync = params.data;
-			sync.chanid = params.to;
-			user.pipe.sendRaw("SYNC", sync);
-		}
-		
-		return {"name":"PUSHED","data":{"value":"ok"}};
+		return ["425", "UNKNOWN_RECIPIENT"];
 	}else{
 		return 0;
 	}
