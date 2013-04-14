33dbb507996e7444193f56e22da84303f3a59468
ptejada <ptejada26@gmail.com>
2013-04-13 20:04:22 -0400

    Hide all user/channel properties but public properties

--- js/src/channel.js
+++ js/src/channel.js
@@ -2,6 +2,90 @@
  * Channel object construtor
  */
 APS.channel = function(pipe, client) {
+	Object.defineProperties(this, {
+	
+		/*
+		 * Update function to update properties object
+		 * This function is used internally to update objects
+		 * when the autoUpdate option is enabled. The function
+		 * checks for a revision number in the object. Properties
+		 * are only updated if they are differnt. This method triggers
+		 * properties specific events which can be ise observe/watch
+		 * property changes 
+		 */
+		update: {
+			value: function(o){
+				o._rev = parseInt(o._rev);
+				if(o._rev > this._rev){
+					for(var i in o){
+						if(this[i] != o[i]){
+							this[i] = o[i];
+							this.trigger("user"+i+"Update",[o[i], this]);
+							this.trigger("userUpdate",[i, o[i], this]);
+						}
+					}
+				}
+			}
+		},
+		
+		/*
+		 * The function makes a user exit/unsubsribe from a channel
+		 * no paramaters are required for this method
+		 */
+		leave: {
+			value: function(){
+				this.trigger("unsub", [client.user, this]);
+				
+				client.sendCmd('LEFT', {"channel": this.name});
+				
+				this.log("Unsubscribed");
+				
+				delete client.channels[this.name];
+			
+				//Delete the Event Queue in case the channel is created again
+				delete client.eQueue[this.name];
+			}
+		},
+		
+		/*
+		 * Add the more critical properties which other
+		 * methods and the framework itself depend on
+		 */
+		_rev: {
+			value: null,
+			configurable: true,
+			writable: true
+		},
+		_events: {
+			value: {},
+			writable: true
+		},
+		_client: {
+			value: client
+		},
+		pubid: {
+			value: pipe.pubid
+		},
+		users: {
+			value: {},
+			writable: true
+		},
+		
+		/*
+		 * Bind event and logging related functions stragiht from the client,
+		 * effectively saving a whole lot of code :)
+		 */
+		on: {
+			value: client.on.bind(this)
+		},
+		trigger: {
+			value: client.trigger.bind(this)
+		},
+		log: {
+			value: client.log.bind(client, "[channel]", "["+this.name+"]")
+		}
+	});
+	
 	/*
 	 * Add all public properties to the root of the object
 	 * for easy access
@@ -11,96 +95,52 @@ APS.channel = function(pipe, client) {
 	}
 	
 	/*
-	 * Add the more critical properties on which other
-	 * methods and the framework itself depends on
-	 */
-	this._events = {};
-	this.pubid = pipe.pubid;
-	this._client = client;
-	
-	/*
-	 * Update function to update properties object
-	 * This function is used internally to update objects
-	 * when the autoUpdate option is enabled. The function
-	 * checks for a revision number in the object. Properties
-	 * are only updated if they are differnt. This method triggers
-	 * properties specific events which can be ise observe/watch
-	 * property changes 
-	 */
-	this.update = function(o){
-		o._rev = parseInt(o._rev);
-		if(o._rev > this._rev){
-			for(var i in o){
-				if(this[i] != o[i]){
-					this[i] = o[i];
-					this.trigger("channel"+i+"Update",[o[i], this]);
-					this.trigger("channelUpdate",[i, o[i], this]);
-				}
-			}
-		}
-	}
-	
-	/*
-	 * The function makes a user exit/unsubsribe from a channel
-	 * no paramaters are required for this method
-	 */
-	this.leave = function(){
-		this.trigger("unsub", [client.user, this]);
-		
-		client.sendCmd('LEFT', {"channel": this.name});
-		
-		this.log("Unsubscribed");
-		
-		delete client.channels[this.name];
-	
-		//Delete the Event Queue in case the channel is created again
-		delete client.eQueue[this.name];
-	}
-	
-	/*
 	 * The following block filters some methods that only apply
 	 * to interactive channels. All channels are consider interactive
 	 * but the ones which name's starts with the asterisk(*) character
 	 */
 	if(this.name.indexOf("*") !== 0){
+		Object.defineProperties(this, {
 		//Methods and prop for interactive channels
-		this.users = {};
-		this.addUser = function(u){
-			var user = client.getPipe[u.pubid];
-			if(!user){
-				/*
-				 * User object does not exists in the client
-				 * Initiate user object and store it
-				 */
-				client.pipes[u.pubid] = new APS.user(u, client);
-				user = client.pipes[u.pubid];
+		users: {
+			value: {},
+			writable: true
+		},
+		addUser: {
+			value: function(u){
+				var user = client.getPipe[u.pubid];
+				if(!user){
+					/*
+					 * User object does not exists in the client
+					 * Initiate user object and store it
+					 */
+					client.pipes[u.pubid] = new APS.user(u, client);
+					user = client.pipes[u.pubid];
+					
+					//Add user's own pipe to channels list
+					user.channels[user.pubid] = user;
+				}else{
+					/*
+					 * User object exists
+					 * Update object if autoUpdate is enabled
+					 */
+					if(client.option.autoUpdate)
+						user.update(u.properties);
+				}
 				
-				//Add user's own pipe to channels list
-				user.channels[user.pubid] = user;
-			}else{
-				/*
-				 * User object exists
-				 * Update object if autoUpdate is enabled
-				 */
-				if(client.option.autoUpdate)
-					user.update(u.properties);
+				//Add channel reference to the user
+				user.channels[this.name] = this;
+				
+				this.users[u.pubid] = user;
+				return user;
 			}
-			
-			//Add channel reference to the user
-			user.channels[this.name] = this;
-			
-			this.users[u.pubid] = user;
-			return user;
+		},
+		send: {
+			value: client.send.bind(client, this.pubid)
+		},
+		pub: {
+			value: client.pub.bind(client, this.name)
 		}
-		this.send = APS.prototype.send.bind(client, this.pubid);
-		this.pub = client.pub.bind(client, this.name);
-	}
+	})}
 	
-	/*
-	 * Bind event and logging related functions stragiht from the client,
-	 * effectively saving a whole lot of code :)
-	 */
-	this.on = client.on.bind(this);
-	this.trigger = client.trigger.bind(this);
-	this.log = client.log.bind(client, "[channel]", "["+this.name+"]");
 }
--- js/src/user.js
+++ js/src/user.js
@@ -2,6 +2,61 @@
  * User object constructor
  */
 APS.user = function(pipe, client){
+	Object.defineProperties(this, {
+	
+		/*
+		 * Update function to update properties object
+		 * This function is used internally to update objects
+		 * when the autoUpdate option is enabled. The function
+		 * checks for a revision number in the object. Properties
+		 * are only updated if they are differnt. This method triggers
+		 * properties specific events which can be ise observe/watch
+		 * property changes 
+		 */
+		update: {
+			value: function(o){
+				o._rev = parseInt(o._rev);
+				if(o._rev > this._rev){
+					for(var i in o){
+						if(this[i] != o[i]){
+							this[i] = o[i];
+							client.trigger("user"+i+"Update",[o[i], this]);
+							client.trigger("userUpdate",[i, o[i], this]);
+						}
+					}
+				}
+			}
+		},
+		
+		/*
+		 * Add the more critical properties which other
+		 * methods and the framework itself depend on
+		 */
+		_rev: {
+			value: null,
+			configurable: true,
+			writable: true
+		},
+		channels: {
+			value: {},
+			writable: true
+		},
+		pubid: {
+			value: pipe.pubid
+		},
+		
+		/*
+		 * Bind event and logging related functions stragiht from the client,
+		 * effectively saving a whole lot of code :)
+		 */
+		pub: {
+			value: client.pub.bind(client, this.pubid)
+		},
+		aend: {
+			value: client.send.bind(client, this.pubid)
+		}
+	});
+	
 	/*
 	 * Add all public properties to the root of the object
 	 * for easy access
@@ -10,47 +65,97 @@ APS.user = function(pipe, client){
 		this[i] = pipe.properties[i]
 	}
 	
-	/*
-	 * Add the more critical properties on which other
-	 * methods depend and the framework itself depends on
-	 */
-	this.pubid = pipe.pubid;
-	this.channels = {};
-	
-	/*
-	 * Import event sending related function straight from the client
-	 * object
-	 */
-	this.pub = APS.prototype.pub.bind(client, this.pubid);
-	this.send = APS.prototype.send.bind(client, this.pubid);
-	
-	/*
-	 * Update function to update properties object
-	 * This function is used internally to update objects
-	 * when the autoUpdate option is enabled. The function
-	 * checks for a revision number in the object. Properties
-	 * are only updated if they are differnt. This method triggers
-	 * properties specific events which can be ise observe/watch
-	 * property changes 
-	 */
-	this.update = function(o){
-		o._rev = parseInt(o._rev);
-		if(o._rev > this._rev){
-			for(var i in o){
-				if(this[i] != o[i]){
-					this[i] = o[i];
-					client.trigger("user"+i+"Update", [o[i], this]);
-					client.trigger("userUpdate",[i, o[i], this]);
-				}
-			}
-		}
-	}
 }
 
 /*
  * Current user object constructor
  */
 APS.cUser = function(pipe, client){
+	Object.defineProperties(this, {
+	
+		/*
+		 * Update function to update properties object
+		 * This function is used internally to update objects
+		 * when the autoUpdate option is enabled. The function
+		 * checks for a revision number in the object. Properties
+		 * are only updated if they are differnt. This method triggers
+		 * properties specific events which can be ise observe/watch
+		 * property changes 
+		 */
+		update: {
+			value: function(o){
+				o._rev = parseInt(o._rev);
+				if(o._rev > this._rev){
+					for(var i in o){
+						if(this[i] != o[i]){
+							this[i] = o[i];
+							this.trigger("user"+i+"Update",[o[i], this]);
+							this.trigger("userUpdate",[i, o[i], this]);
+						}
+					}
+				}
+			}
+		},
+		
+		/*
+		 * Change or upadte a property in the object and send it to the
+		 * server for propagation. In order for this method to work 
+		 * properly the option autoUpdate should enable
+		 */
+		change: {
+			value: function(name, value){
+				if(typeof name == "object"){
+					var data = name;
+				}else{
+					var data = {};
+					data[name] = value;
+				}
+				//NOTE: data has no revision number thus update will fail
+				//this.update(data);
+				this._client.sendCmd("propUpdate", data);
+			}
+		},
+	
+	
+		/*
+		 * Add the more critical properties which other
+		 * methods and the framework itself depend on
+		 */
+		_rev: {
+			value: null,
+			configurable: true,
+			writable: true
+		},
+		_events: {
+			value: {},
+			writable: true
+		},
+		_client: {
+			value: client
+		},
+		pubid: {
+			value: pipe.pubid
+		},
+		channels: {
+			value: {},
+			writable: true
+		},
+		
+		/*
+		 * Bind event and logging related functions stragiht from the client,
+		 * effectively saving a whole lot of code :)
+		 */
+		on: {
+			value: client.on.bind(this)
+		},
+		trigger: {
+			value: client.trigger.bind(this)
+		},
+		log: {
+			value: client.log.bind(client, "[CurrentUser]")
+		}
+	});
+	
 	/*
 	 * Add all public properties to the root of the object
 	 * for easy access
@@ -58,60 +163,4 @@ APS.cUser = function(pipe, client){
 	for(var i in pipe.properties){
 		this[i] = pipe.properties[i]
 	}
-	
-	/*
-	 * Update function to update properties object
-	 * This function is used internally to update objects
-	 * when the autoUpdate option is enabled. The function
-	 * checks for a revision number in the object. Properties
-	 * are only updated if they are differnt. This method triggers
-	 * properties specific events which can be ise observe/watch
-	 * property changes 
-	 */
-	this.update = function(o){
-		o._rev = parseInt(o._rev);
-		if(o._rev > this._rev){
-			for(var i in o){
-				if(this[i] != o[i]){
-					this[i] = o[i];
-					this.trigger("user"+i+"Update",[o[i], this]);
-					this.trigger("userUpdate",[i, o[i], this]);
-				}
-			}
-		}
-	}
-	
-	/*
-	 * Change or upadte a property in the object and send it to the
-	 * server for propagation. In order for this method to work 
-	 * properly the option autoUpdate should enable
-	 */
-	this.change = function(name, value){
-		if(typeof name == "object"){
-			var data = name;
-		}else{
-			var data = {};
-			data[name] = value;
-		}
-		//NOTE: data has no revision number thus update will fail
-		//this.update(data);
-		this._client.sendCmd("propUpdate", data);
-	}
-	
-	/*
-	 * Add the more critical properties on which other
-	 * methods and the framework itself depend on
-	 */
-	this._events = {};
-	this._client = client;
-	this.pubid = pipe.pubid;
-	this.channels = {};
-	
-	/*
-	 * Bind event and logging related functions stragiht from the client,
-	 * effectively saving a whole lot of code :)
-	 */
-	this.on = client.on.bind(this);
-	this.trigger = client.trigger.bind(this);
-	this.log = client.log.bind(client, "[CurrentUser]");
 }
