cba721da3a3297f738baa810e5ab3591d6d09883
Pablo Tejada <ptejada26@gmail.com>
2013-08-25 14:09:45 -0400

    Added new built-in channel event restored

--- APE_Server/APScripts/framework/cmd_restore.js
+++ APE_Server/APScripts/framework/cmd_restore.js
@@ -6,5 +6,13 @@ Ape.registerCmd("RESTORE", true, function(params, info){
 	 * Since this is the first request the Server will 
 	 * automatically sent the user user IDENT and LOGIN raws
 	 */
+
+	/*
+	 * Trigger the restored events on user's channels
+	 */
+	for(var name in info.user.channels){
+		var channel = info.user.channels[name];
+		Ape.triggerChannelEvent(channel, "restored", [info.user, channel]);
+	}
 	return 1;
 });
