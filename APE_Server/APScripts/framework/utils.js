bec24519dbc3228d23e324218e2af67f29e57a9d
Pablo Tejada <ptejada26@gmail.com>
2013-08-25 14:05:19 -0400

    Enhanced Ape.log() function

--- APE_Server/APScripts/framework/utils.js
+++ APE_Server/APScripts/framework/utils.js
@@ -5,14 +5,63 @@ Ape.userlist = {};
 Ape.channelEvents = {};
 
 /*
- * Enchance Ape.log function
+ * Enhance Ape.log function
  */
 Apelog = Ape.log;
 Ape.log = function(data){
 	switch(typeof data){
 		case "object":
-		//case "array":
-			Apelog(data.toSource());
+			var result = "----" + data + "----\n";
+			var level = 0;
+			data = data.toSource();
+
+			for(var i= 0; data.length > i; i++){
+				var letter = data[i];
+				var next = data[i+1];
+				var previous = data[i-1];
+
+				var tab = false;
+
+				if(letter != " "){
+					result += letter;
+
+					switch(letter){
+						case "{":
+							if(next != "}"){
+								result += "\n";
+								level++;
+								tab = true;
+							}
+							break;
+						case "}":
+							if(previous != "{" && next != "," && next != ")"){
+								result += "\n";
+								level--;
+								tab = true;
+							}
+							break;
+						case ",":
+							result += "\n";
+							tab = true;
+							break
+						case ":":
+							result += " ";
+							break;
+
+						default:
+							if(next == "}"){
+								result += "\n";
+								level--;
+								tab = true;
+							}
+
+					}
+					if(tab)
+						for(var l=0; level > l; l++)
+							result += "  ";
+				}
+			}
+			Apelog(result);
 			break;
 		default:
 			Apelog(data);
